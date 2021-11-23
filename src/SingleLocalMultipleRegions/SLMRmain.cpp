162db88346b06be20faac6976f1ff9bad986accf
Florian Sattler <vuld3r@gmail.com>
2021-11-23 10:32:00 +0100

    Adds additional test program with multiple regions

--- src/SingleLocalMultipleRegions/SLMRmain.cpp
+++ src/SingleLocalMultipleRegions/SLMRmain.cpp
@@ -11,10 +11,26 @@ int main(int argc, char *argv[]) {
     Slow = true;
   }
 
+  // Multiple regions related to --slow that take different amounts of time.
+
   if (Slow) {
     fpcsc::sleep_for_secs(5);
   } else {
     fpcsc::sleep_for_secs(2);
+  }
+
+  fpcsc::sleep_for_secs(3); // General waiting time
+
+  if (Slow) {
+    fpcsc::sleep_for_secs(3);
+  } else {
+    fpcsc::sleep_for_secs(1);
+  }
+
+  fpcsc::sleep_for_secs(4); // General waiting time
+
+  if (Slow) {
+    fpcsc::sleep_for_secs(6);
   }
 
   return 0;
