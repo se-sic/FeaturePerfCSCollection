301238b61c132fc1c1ef270d1e1d0be59bcc5236
Jonas Kaufmann <jokau03@gmail.com>
2022-02-18 17:47:18 +0100

    fixes typo

--- src/SimpleLoopedFeature/SLFmain.cpp
+++ src/SimpleLoopedFeature/SLFmain.cpp
@@ -10,8 +10,8 @@ int main(int argc, char *argv[]) {
   std::cout << "Number of Iterations: " << NumIterations << "\n";
 
   long int SleepFor;
-  fpcsc::getFeatureValue(argc, argv, std::string("--sleepns"), &SleepFor);
-  std::cout << "Nanoseconds to sleep: " << SleepFor << "\n";
+  fpcsc::getFeatureValue(argc, argv, std::string("--sleepms"), &SleepFor);
+  std::cout << "Milliseconds to sleep: " << SleepFor << "\n";
 
   for (int i = 0; i < NumIterations; ++i) {
     fpcsc::sleep_for_millisecs (SleepFor);
