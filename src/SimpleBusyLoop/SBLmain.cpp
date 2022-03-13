4f97ab5c8aeb1ca012a50b9180fb76567fcb363a
Jonas Kaufmann <jokau03@gmail.com>
2022-03-13 12:37:17 +0100

    Rename loop binaries.

--- src/SimpleBusyLoop/SBLmain.cpp
+++ src/SimpleBusyLoop/SBLmain.cpp
@@ -5,19 +5,20 @@
 
 volatile long Counter;
 
-int main(int argc, char *argv[]) {
-
-  // Feature NumIteration
+int main(int argc, char *argv[])
+{
   long NumIterations = fpcsc::getRequiredFeatureValue(argc, argv, std::string("--iterations"));
   std::cout << "Number of Iterations: " << NumIterations << "\n";
 
-  // Feature CountTo
   long CountTo = fpcsc::getRequiredFeatureValue(argc, argv, std::string("--count_to"));
   std::cout << "Count to " << CountTo << " in every iteration.\n";
 
-  for (int i = 0; i < NumIterations; ++i) {
-    if (CountTo) {
-      for (Counter = 0; Counter < CountTo; ++Counter);
+  for (int i = 0; i < NumIterations; ++i)
+  {
+    if (CountTo)
+    {
+      for (Counter = 0; Counter < CountTo; ++Counter)
+        ;
     }
   }
 
