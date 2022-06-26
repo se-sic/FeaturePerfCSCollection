61e270892d42d7bbfae3ff8b171f464ac977d96a
Daniel Gusenburger <danielgusenburger@online.de>
2022-06-26 20:41:46 +0200

    added new folder for bubbling experiments

---
+++ src/MultipleSimpleInstructions/MSImain.cpp
@@ -0,0 +1,23 @@
+#include "fpcsc/perf_util/sleep.h"
+
+#include <string>
+
+int main(int argc, char *argv[]) {
+
+  bool Slow = false;
+
+  if ((argc >= 2 && argv[1] == std::string("--slow")) ||
+      (argc >= 3 && argv[2] == std::string("--slow"))) {
+    Slow = true;
+  }
+
+	int a = 0;
+	++a;
+  if (Slow) {
+    fpcsc::sleep_for_secs(5);
+  } else {
+    fpcsc::sleep_for_secs(2);
+  }
+
+  return 0;
+}
