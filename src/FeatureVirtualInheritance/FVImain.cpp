59c724d745bc2a778599ccfd1495d445d3b82730
Florian Sattler <vuld3r@gmail.com>
2022-10-30 11:42:11 +0100

    Adapts case study to use new feature attributes for classes

--- src/FeatureVirtualInheritance/FVImain.cpp
+++ src/FeatureVirtualInheritance/FVImain.cpp
@@ -1,37 +1,39 @@
-#include "fpcsc/perf_util/sleep.h"
 #include "fpcsc/perf_util/feature_cmd.h"
+#include "fpcsc/perf_util/sleep.h"
 
-class FeatureGroup {
+class __attribute__((feature_variable("FeatureGroup"))) FeatureGroup {
 public:
-  virtual void doWork() {
-    fpcsc::sleep_for_secs(5);
-  }
+  virtual void doWork() { fpcsc::sleep_for_secs(5); }
 
   virtual ~FeatureGroup() = default;
 };
 
-class FValue1 : public FeatureGroup {
-  void doWork() override {
-    fpcsc::sleep_for_secs(1);
-  }
+class __attribute__((feature_variable("FValue1"))) FValue1
+    : public FeatureGroup {
+public:
+  void doWork() override { fpcsc::sleep_for_secs(1); }
 };
 
-class FValue2 : public FeatureGroup {
-  void doWork() override {
-    fpcsc::sleep_for_secs(2);
-  }
+class __attribute__((feature_variable("FValue2"))) FValue2
+    : public FeatureGroup {
+public:
+  void doWork() override { fpcsc::sleep_for_secs(2); }
 };
 
-class FValue3 : public FeatureGroup {
-  void doWork() override {
-    fpcsc::sleep_for_secs(3);
-  }
+class __attribute__((feature_variable("FValue3"))) FValue3
+    : public FeatureGroup {
+public:
+  void doWork() override { fpcsc::sleep_for_secs(3); }
 };
 
-class FValueOther : public FeatureGroup {};
+// This should not appear in traces as there is no implementation different
+// from the base class
+class __attribute__((feature_variable("FValueOther"))) FValueOther
+    : public FeatureGroup {};
 
 int main(int argc, char *argv[]) {
-  FeatureGroup *feature = nullptr;
+  FeatureGroup *feature __attribute__((feature_variable("FG_variable"))) =
+      nullptr;
 
   if (fpcsc::isFeatureEnabled(argc, argv, std::string("--value1"))) {
     feature = new FValue1();
@@ -43,13 +45,10 @@ int main(int argc, char *argv[]) {
     feature = new FValueOther();
   }
 
-
   if (feature) {
     feature->doWork(); // Feature dependent time cost
 
     delete feature;
   }
-
-
   return 0;
 }
