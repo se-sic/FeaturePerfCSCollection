0b5bd0ef7995dd4b9bf6ede16db74917c1b8b857
Aufrichtig98 <manuelmesserig@gmail.com>
2022-11-24 17:49:52 +0100

    extended experiment by one feature

--- src/FeatureCallingFeature/FeatureCallingFeature.cpp
+++ src/FeatureCallingFeature/FeatureCallingFeature.cpp
@@ -4,14 +4,25 @@
 #include <iostream>
 #include <string>
 
-void featureB(){
-  fpcsc::sleep_for_secs(2);
+void featureC(bool c){
+
+ if(!c)
+   fpcsc::sleep_for_secs(2); // If a=1, b=1, c=1 or b=1, c=1
+  else
+    fpcsc::sleep_for_secs(1); // If a=1, b=1, c=0 or b=1, c=0
+
 }
 
-void featureA(bool b){
+void featureB(bool c){
+  fpcsc::sleep_for_secs(2);
+
+  featureC(c);
+}
+
+void featureA(bool b, bool c){
   fpcsc::sleep_for_secs(2);
     if (b) {
-      featureB();
+      featureB(c);
     }
 }
 
@@ -23,6 +34,7 @@ int main(int argc, char *argv[]) {
 
   bool a = false;
   bool b = false;
+  bool c = false;
 
   if (fpcsc::isFeatureEnabled(argc, argv, std::string("--a"))) {
     a = true;
@@ -30,19 +42,40 @@ int main(int argc, char *argv[]) {
   if (fpcsc::isFeatureEnabled(argc, argv, std::string("--b"))) {
     b = true;
   }
-
-  fpcsc::sleep_for_secs(2); //spending time in base feature
-
-  if (a)
-    featureA(b);
-  if (b)
-    featureB();
-
-  if (a && b){
-    featureAB();
+  if (fpcsc::isFeatureEnabled(argc, argv, std::string("--c"))) {
+    c = true;
   }
 
+  fpcsc::sleep_for_secs(1); //spending time in base feature
+
+  if (a)
+    featureA(b,c);
+  featureC(c);
+
+
+  if (a && b){
+    featureAB(); // Enters function once
+  }
+
+  if (a && b){
+    fpcsc::sleep_for_secs(1);
+  }
+
+  fpcsc::sleep_for_secs(1); //spending time in base feature
+
+  if(a && b && c){
+    fpcsc::sleep_for_secs(1);
+  }
 
   return 0;
 }
-
+/* Expected results:
+ * {} = 2s
+ * a = 6s
+ * b = 6s
+ * a, b = 2 + 6 + 2 + 4  = 14
+ * a, c = 5s
+ * b, c = 5s
+ * a, b, c = 2 + 5 + 1 + 4 + 1 = 13
+ *
+ * */
