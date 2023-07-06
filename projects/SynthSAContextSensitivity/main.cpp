82b3fcc626ea4c66a16e6fefbe5276b839afc0dd
Florian Sattler <vuld3r@gmail.com>
2023-07-06 20:17:00 +0200

    Adds empty new example project

---
+++ projects/SynthSAContextSensitivity/main.cpp
@@ -0,0 +1,8 @@
+#include "fpcsc/perf_util/feature_cmd.h"
+
+int main(int argc, char *argv[]) {
+  bool __attribute__((feature_variable("Encryption"))) UseEncryption = false;
+  bool __attribute__((feature_variable("Compression"))) UseCompression = false;
+
+  return 0;
+}
