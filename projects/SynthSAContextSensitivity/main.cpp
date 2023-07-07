0c1c4d903c0ebe2f0f42a04f99c14d982d5b2b42
Florian Sattler <vuld3r@gmail.com>
2023-07-07 11:44:59 +0200

    Adapts case study to work without C++20 and builtin code

--- projects/SynthSAContextSensitivity/main.cpp
+++ projects/SynthSAContextSensitivity/main.cpp
@@ -8,23 +8,52 @@
 
 /// This synthetic case study depcits a small computation tool that
 ///
-/// The key idea is that lowerBoundPowerOfTwo needs a context sensitive static
+/// The key idea is that boundsChecks needs a context sensitive static
 /// analysis, otherwise the non-feature-related usages would be detected and
 /// instrumented as well.
 
+unsigned countLeadingZeros(unsigned Val) {
+  if (!Val)
+    return std::numeric_limits<unsigned>::digits;
+
+  // Bisection method.
+  unsigned ZeroBits = 0;
+  for (unsigned Shift = std::numeric_limits<unsigned>::digits >> 1; Shift;
+       Shift >>= 1) {
+    unsigned Tmp = Val >> Shift;
+    if (Tmp)
+      Val = Tmp;
+    else
+      ZeroBits |= Shift;
+  }
+  return ZeroBits;
+}
+
 unsigned lowerBoundPowerOfTwo(unsigned Val) {
-  // TODO: check if the analysis can look through this call
-  return 1 << (std::numeric_limits<unsigned>::digits - 1 - __builtin_clzl(Val));
+  return 1 << (std::numeric_limits<unsigned>::digits - 1 -
+               countLeadingZeros(Val));
+}
+
+__attribute__((noinline)) unsigned boundsChecks(unsigned Val) {
+  if (Val > 32768) {
+    return 32768;
+  };
+
+  return Val; // Can be feature dependent
 }
 
 unsigned compute(unsigned BaseValue, unsigned Bound) {
-  unsigned PowOf2 = lowerBoundPowerOfTwo(Bound);
+  unsigned CheckedBound = boundsChecks(Bound);
+  unsigned CheckedBaseValue = boundsChecks(BaseValue);
+
+  unsigned PowOf2 = lowerBoundPowerOfTwo(CheckedBound);
   // Delay compute (indirectly based on MemoryLimit)
   fp_util::sleep_for_millisecs(PowOf2 * 100);
 
-  fp_util::sleep_for_millisecs(BaseValue * Bound * 2);
+  // Delay compute (Based on user input and indirectly on MemoryLimit)
+  fp_util::sleep_for_millisecs(CheckedBaseValue * CheckedBound * 2);
 
-  return BaseValue * Bound;
+  return CheckedBaseValue * CheckedBound;
 }
 
 int main(int argc, char *argv[]) {
@@ -39,14 +68,14 @@ int main(int argc, char *argv[]) {
 
   uint64_t Result{};
 
-  if (lowerBoundPowerOfTwo(MemoryLimit) < 256) { // 0 =< x < 256
+  if (boundsChecks(MemoryLimit) < 256) { // 0 =< x < 256
     if (Compress) {
       Result = compute(UserInput, 9); // leads to less imprecise results
     } else {
       Result = compute(UserInput, 8); // leads to imprecise results
     }
-  } else if (lowerBoundPowerOfTwo(MemoryLimit) < 1024) { // 256 =< x < 1024
-    Result = compute(UserInput, 16); // leads to imprecise results
+  } else if (boundsChecks(MemoryLimit) < 1024) { // 256 =< x < 1024
+    Result = compute(UserInput, 16);             // leads to imprecise results
   } else {
     Result = compute(UserInput, 250);
   }
