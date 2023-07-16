8820ab9063464e6abfa69d5a8572d11d8c2c65a9
Florian Sattler <vuld3r@gmail.com>
2023-07-16 12:42:44 +0200

    Start work on whole program case study

--- projects/SynthSAInterProcedural/main.cpp
+++ projects/SynthSAInterProcedural/main.cpp
@@ -1,1 +1,18 @@
-int main(/* int argc, char *argv[] */) { return 0; }
+#include "fp_util/feature_cmd.h"
+#include "fp_util/sleep.h"
+
+#include <cstdio>
+
+/// This synthetic case study for whole program analysis
+///
+/// The key idea is that configuration code is spread all over the code base,
+/// an inter-procedural whole program analysis.
+
+int main(int argc, char *argv[]) {
+
+  uint64_t Result{};
+
+  fp_util::sleep_for_secs(3);
+  std::printf("Result: %lu\n", Result);
+  return 0;
+}
