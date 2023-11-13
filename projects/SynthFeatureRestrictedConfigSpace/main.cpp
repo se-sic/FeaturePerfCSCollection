b112292305989d53c35d1a7bdc4e5e5ff151b29a
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2023-11-13 13:22:26 +0100

    Remove highlight markers

--- projects/SynthFeatureRestrictedConfigSpace/main.cpp
+++ projects/SynthFeatureRestrictedConfigSpace/main.cpp
@@ -76,8 +76,6 @@ int main(int argc, char *argv[]) {
     fp_util::sleep_for_millisecs(20);
   }
 
-  ___REGION_START __RT_High "HotCode"
   fp_util::sleep_for_millisecs(sleepTime);
-  ___REGION_END __RT_High "HotCode"
   return 0;
 }
