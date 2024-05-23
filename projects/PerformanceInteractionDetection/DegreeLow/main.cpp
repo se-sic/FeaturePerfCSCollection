f6b2548c8039ee21d017b741a8e181a1e9b25cd6
Sebastian Böhm <boehmseb@cs.uni-saarland.de>
2024-05-23 09:24:07 +0200

    Add interaction-degree case studies

---
+++ projects/PerformanceInteractionDetection/DegreeLow/main.cpp
@@ -0,0 +1,39 @@
+#include "fp_util/feature_cmd.h"
+#include "fp_util/sleep.h"
+
+/// Low interaction degree case study:
+/// Regression is triggered by a single feature.
+
+bool __attribute__((feature_variable("F1"))) F1;
+bool __attribute__((feature_variable("F2"))) F2;
+bool __attribute__((feature_variable("F3"))) F3;
+bool __attribute__((feature_variable("F4"))) F4;
+bool __attribute__((feature_variable("F5"))) F5;
+bool __attribute__((feature_variable("F6"))) F6;
+bool __attribute__((feature_variable("F7"))) F7;
+bool __attribute__((feature_variable("F8"))) F8;
+bool __attribute__((feature_variable("F9"))) F9;
+bool __attribute__((feature_variable("F10"))) F10;
+
+
+int main(int argc, char *argv[]) {
+  F1 = fp_util::isFeatureEnabled(argc, argv, "--f1");
+  F2 = fp_util::isFeatureEnabled(argc, argv, "--f2");
+  F3 = fp_util::isFeatureEnabled(argc, argv, "--f3");
+  F4 = fp_util::isFeatureEnabled(argc, argv, "--f4");
+  F5 = fp_util::isFeatureEnabled(argc, argv, "--f5");
+  F6 = fp_util::isFeatureEnabled(argc, argv, "--f6");
+  F7 = fp_util::isFeatureEnabled(argc, argv, "--f7");
+  F8 = fp_util::isFeatureEnabled(argc, argv, "--f8");
+  F9 = fp_util::isFeatureEnabled(argc, argv, "--f9");
+  F10 = fp_util::isFeatureEnabled(argc, argv, "--f10");
+
+  int t = 10;
+
+  if (F1) {
+    t *= 2;
+  }
+
+  fp_util::sleep_for_millisecs(100 + t);
+  return 0;
+}
